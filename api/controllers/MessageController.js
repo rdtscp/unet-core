25ed2e9516874219110dd10c16e04165e2cb97a0
Alexander Wilson <acwilson96@gmail.com>
2017-09-02 18:57:24 +0100

    Able to send, and skeleton for messages.

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -7,5 +7,58 @@
 
 module.exports = {
     
+    /* 'post /unet/message/get'
+     * Returns a message requested by its number in a Chat.
+     * 
+     * Returns json:
+     * {
+     *     err: [ true | false ],
+     *     warning: [ true | false ],
+     *     msg: Error, Warning or Success message; E.G. [ 'Failed to retrieve message.' ],
+     *     message: Message.js model
+     * }
+     */
+    get: function (req, res) {
+        
+    },
+
+    /* 'post /unet/message/create'
+     * Creates a message from the current user to a chat id.
+     * 
+     * Returns json:
+     * {
+     *     err: [ true | false ],
+     *     warning: [ true | false ],
+     *     msg: Error, Warning or Success message; E.G. [ 'Failed to create a message.' ],
+     *     message: Message.js model
+     * }
+     */
+    create: function (req, res) {
+        // Parse POST for params.
+        var chatID  = req.param('id');
+        var msg     = req.param('message');
+        var userID  = req.options.userid;
+        Message.create({
+            chat: chatID,
+            message: msg
+        }).exec((err, message) => {
+            if (err) return res.json(Utils.return_error(err));
+            if (message) {
+                return res.json({
+                    err: false,
+                    warning: false,
+                    msg: 'Message sent',
+                    message: message
+                });
+            } else {
+                return res.json({
+                    err: false,
+                    warning: true,
+                    msg: 'Message was not created succesfully.'
+                });
+            }
+        });
+    }
+
 };
 
