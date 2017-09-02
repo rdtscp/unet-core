a01f4816f499ff740836ee0fd758d64556786234
Alexander Wilson <acwilson96@gmail.com>
2017-09-02 13:41:16 +0100

    Skeleton for ChatController

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -7,7 +7,25 @@
 
 module.exports = {
 
-    
+    getall: function (req,res) {
+
+    },
+
+    get: function (req, res) {
+
+    },
+
+    create: function (req, res) {
+
+    },
+
+    destroy: function (req, res) {
+
+    },
+
+    update: function (req, res) {
+
+    }
 	
 };
 
