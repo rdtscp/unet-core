46cdbca0b9d4ca225417cde713e01e93af788f55
Alex Wilson <acwilson96@protonmail.com>
2017-09-02 14:07:14 +0100

    FriendshipController using policies now.

--- api/controllers/FriendshipController.js
+++ api/controllers/FriendshipController.js
@@ -30,26 +30,20 @@ module.exports = {
      */
     getall: function (req, res) {
         var authToken     = req.param('token');
-        Device.findOne({
-            token: authToken
-        }).exec((err, device) => {
+        var userID        = req.options.userid;
+        Friendship.find({
+            or: [
+                {receiver: userID},
+                {sender: userID, confirmed: true}
+            ]
+        }).exec((err, friendships) => {
             if (err) return res.json(Utils.return_error(err));
-            if (device) {
-                Friendship.find({
-                    or: [
-                        {receiver: device.owner},
-                        {sender: device.owner, confirmed: true}
-                    ]
-                }).exec((err, friendships) => {
-                    if (err) return res.json(Utils.return_error(err));
-                    return res.json({
-                        err: false,
-                        warning: false,
-                        message: null,
-                        friendships: friendships
-                    })
-                });
-            }
+            return res.json({
+                err: false,
+                warning: false,
+                message: null,
+                friendships: friendships
+            })
         });
     },
 
@@ -67,20 +61,16 @@ module.exports = {
     get: function (req, res) {
         var authToken     = req.param('token');
         var friendshipID  = req.param('id');
-        Device.findOne({
-            token: authToken
-        }).exec((err, device) => {
-            Friendship.findOne({
-                id: id
-            }).exec((err, friendship) => {
-                if (err) return res.json(Utils.return_error(err));
-                return res.json({
-                    err: false,
-                    warning: false,
-                    message: null,
-                    friendship: friendship
-                })
-            });
+        Friendship.findOne({
+            id: id
+        }).exec((err, friendship) => {
+            if (err) return res.json(Utils.return_error(err));
+            return res.json({
+                err: false,
+                warning: false,
+                message: null,
+                friendship: friendship
+            })
         });
     },
     
@@ -99,37 +89,23 @@ module.exports = {
         // Parse POST for User params.
         var authToken     = req.param('token');
         var requestedUser = req.param('username');
-        // Check if current request is authenticated.
-        Device.findOne({
-            token: authToken
-        }).exec((err, device) => {
+        var userID        = req.options.userid;
+        // Check Users required for this Friendship exist.
+        Friendship.getUsers({id: userID}, {username: requestedUser}, (err, sender, receiver) => {
             if (err) return res.json(Utils.return_error(err));
-            if (device) {
-                // Check Users required for this Friendship exist.
-                Friendship.getUsers({id: device.owner}, {username: requestedUser}, (err, sender, receiver) => {
+            if (sender && receiver) {
+                // Create new Friendship.
+                Friendship.create({
+                    sender: sender.id,
+                    receiver: receiver.id
+                }).exec((err, newFriendship) => {
                     if (err) return res.json(Utils.return_error(err));
-                    if (sender && receiver) {
-                        // Create new Friendship.
-                        Friendship.create({
-                            sender: sender.id,
-                            receiver: receiver.id
-                        }).exec((err, newFriendship) => {
-                            if (err) return res.json(Utils.return_error(err));
-                            return res.json({
-                                err: false,
-                                warning: false,
-                                msg: sent_request_msg,
-                                friendship: newFriendship
-                            });
-                        });
-                    } else {
-                        return res.json({
-                            err: false,
-                            warning: true,
-                            msg: device_unauth_msg,
-                            friendship: null
-                        });
-                    }
+                    return res.json({
+                        err: false,
+                        warning: false,
+                        msg: sent_request_msg,
+                        friendship: newFriendship
+                    });
                 });
             } else {
                 return res.json({
@@ -158,39 +134,25 @@ module.exports = {
         // Parse POST for User params.
         var authToken     = req.param('token');
         var requestedUser = req.param('username');
-        // Check if current request is authenticated.
-        Device.findOne({
-            token: authToken
-        }).exec((err, device) => {
+        var userID        = req.options.userid;
+        // Check Users required for this Friendship exist.
+        Friendship.getUsers({id: userID}, {username: requestedUser}, (err, sender, receiver) => {
             if (err) return res.json(Utils.return_error(err));
-            if (device) {
-                // Check Users required for this Friendship exist.
-                Friendship.getUsers({id: device.owner}, {username: requestedUser}, (err, sender, receiver) => {
+            if (sender && receiver) {
+                // Destroy Friendship.
+                Friendship.destroy({
+                    or: [
+                        {sender: sender.id, receiver: receiver.id},
+                        {sender: receiver.id, receiver: sender.id}
+                    ]
+                }).exec((err, destroyedFriendship) => {
                     if (err) return res.json(Utils.return_error(err));
-                    if (sender && receiver) {
-                        // Destroy Friendship.
-                        Friendship.destroy({
-                            or: [
-                                {sender: sender.id, receiver: receiver.id},
-                                {sender: receiver.id, receiver: sender.id}
-                            ]
-                        }).exec((err, destroyedFriendship) => {
-                            if (err) return res.json(Utils.return_error(err));
-                            return res.json({
-                                err: false,
-                                warning: false,
-                                msg: friend_removed_msg,
-                                friendship: destroyedFriendship
-                            });
-                        });
-                    } else {
-                        return res.json({
-                            err: false,
-                            warning: true,
-                            msg: device_unauth_msg,
-                            friendship: null
-                        });
-                    }
+                    return res.json({
+                        err: false,
+                        warning: false,
+                        msg: friend_removed_msg,
+                        friendship: destroyedFriendship
+                    });
                 });
             } else {
                 return res.json({
