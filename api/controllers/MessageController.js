bca245572000eedfc2f9dc51f416345f06c4c1db
Alexander Wilson <acwilson96@gmail.com>
2017-09-02 17:31:50 +0100

    Added Message model and controller.

---
+++ api/controllers/MessageController.js
@@ -0,0 +1,11 @@
+/**
+ * MessageController
+ *
+ * @description :: Server-side logic for managing Messages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    
+};
+
