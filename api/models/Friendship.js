bb47729fc46e69dcee4012576ca2ad4c3096773d
Alexander Wilson <acwilson96@gmail.com>
2017-09-05 00:52:25 +0100

    Friendship now links two User models.

--- api/models/Friendship.js
+++ api/models/Friendship.js
@@ -18,57 +18,16 @@ module.exports = {
     },
 
     sender: {
-      type: 'string',
+      model: 'User',
       required: true
     },
 
     receiver: {
-      type: 'string',
+      model: 'User',
       required: true
     },
 
-    // Function that returns the user requesting this data's friend username.
-    friend: function (currUserID) {
-      switch (currUserID) {
-        case this.sender:
-          return this.receiver;
-        default:
-          return this.sender;
-      }
-    }
-
   },
-
-  // Returns input list with new attribute 'friend' which contains the model for the friend.
-  getFriend(currUser, friendships, cb) {
-      var tasks = [];
-      var output = [];
-      for (var i=0; i < friendships.length; i++) {
-          var friendship = friendships[i];
-          switch (currUser.id) {
-              // If the requesting user is the sender, set 'friend' to the receiver.
-              case friendship.sender:
-                  var editedFriendship = friendship;
-                  editedFriendship.friend = editedFriendship.receiver;
-                  tasks.push(
-                      output.push(editedFriendship)
-                  );
-              // Else the requesting user is the receiver, set 'friend' to the sender.
-              default:
-                  var editedFriendship = friendship;
-                  editedFriendship.friend = editedFriendship.sender;
-                  tasks.push(
-                      output.push(editedFriendship)
-                  );
-          }
-      }
-      return Promise
-      .all(tasks)
-      .then(result => {
-          cb(output);
-      });
-  },
-
 
   /*
    * Method takes two users, and returns if a Friendship exists between them.
