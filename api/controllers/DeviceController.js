6d136635496c5b2553fe670790847323fb2f83f3
Alex Wilson <acwilson96@protonmail.com>
2017-09-05 20:46:42 +0100

    Device controller now responsible for Auth.

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -5,11 +5,18 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+const bcrypt = require('bcrypt');
+
 module.exports = {
     
-    /*
+    /*' post /unet/device/get'
      * Query to check and see if a device auth token is still valid.
      * 
+     * Returns json:
+     * {
+     *     err: [ true | false ],
+     *     tokenValid: [ true | false ]
+     * }
      */
     get: function (req, res) {
         // Parse POST for User params.
@@ -25,8 +32,69 @@ module.exports = {
                 return res.json({tokenValid: false});
             }
         });
+    },
+
+    /* 'post /unet/device/create'
+     * Submit User credentils to create a Device authenticated to the supplied credentials.
+     * 
+     * Returns json:
+     * {
+     *     err: [ true | false ],
+     *     warning: [ true | false ],
+     *     msg: Error, Warning or Success message; E.G. [ 'Incorrect username or password' ],
+     *     exists: [ true | false ],
+     *     token: Authentication token
+     * }
+     */
+    create: function (req, res) {
+        // Parse POST for User params.
+        var uname  = req.param('username');
+        var pword  = req.param('password');
+
+        // Look up User.
+        User.findOne({
+            username: uname
+        }).exec((err, user) => {
+            if (err) return res.json(Utils.return_error(err));
+            if (user) {
+                // Check Password matches database password.
+                bcrypt.compare(pword, user.password, (err, match) => {
+                    if (err) return res.json(Utils.return_error(err));
+                    if (match) {
+                        User.authenticateDevice(user, req.ip, req.headers['user-agent'], (err, token) => {
+                            if (err) return res.json(Utils.return_error(err));
+                            if (token) {
+                                // Return empty response.
+                                return res.json({
+                                    err: false,
+                                    warning: false,
+                                    msg: null,
+                                    token: token
+                                });
+                            } else {
+                                return res.json({
+                                    err: true,
+                                    warning: true,
+                                    msg: '500 Server Error: Device has not been authenticated.'
+                                });
+                            }
+                        });
+                    } else {
+                        return res.json({
+                            err: false,
+                            warning: true,
+                            msg: get_failure_msg
+                        });
+                    }
+                });
+            } else {
+                return res.json({
+                    err: false,
+                    warning: true,
+                    msg: get_failure_msg
+                });
+            }
+        });
     }
         
 };
-    
-    
