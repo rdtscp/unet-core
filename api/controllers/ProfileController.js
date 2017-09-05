7e5b3d2a478783044950b6fecdfae724d64aeb13
Alex Wilson <acwilson96@protonmail.com>
2017-09-05 21:04:46 +0100

    Profile model is now a purely public model.

--- api/controllers/ProfileController.js
+++ api/controllers/ProfileController.js
@@ -7,19 +7,40 @@
 
 module.exports = {
 
+    /* 'post /unet/profile/get'
+     * Gets a Profile model under the requested ID.
+     * 
+     * Returns json:
+     * {
+     *     err: [ true | false ],
+     *     warning: [ true | false ],
+     *     msg: Error, Warning or Success message; E.G. [ 'Account Deleted.' ],
+     *     exists: [ true | false ],
+     *     profile: Profile.js model
+     * }
+     *
+     */
     get: function (req, res) {
-        var authToken     = req.param('token');
-        var id            = req.param('id');
+        var authToken   = req.param('token');
+        var id          = req.param('id');
         Profile.findOne({
-            owner: id
+            id: id
         }).exec((err, profile) => {
             if (err) return res.json(Utils.return_error(err));
-            return res.json({
-                err: false,
-                warning: false,
-                message: null,
-                profile: profile
-            });
+            if (profile) {
+                return res.json({
+                    err: false,
+                    warning: false,
+                    message: null,
+                    profile: profile
+                });
+            } else {
+                return res.json({
+                    err: false,
+                    warning: true,
+                    message: 'Profile does not exist for current user.'
+                });
+            }
         });
     }
 	
