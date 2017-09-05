a77477c4419d1e99416021a12d52e04c12dd4af9
Alexander Wilson <acwilson96@gmail.com>
2017-09-05 21:05:48 +0100

    Chats will now be tied to User models instead of Profile models.

--- api/models/User.js
+++ api/models/User.js
@@ -19,6 +19,12 @@ module.exports = {
             via: 'owner'
         },
 
+        // Contains a list of Chat models this Profile is a member of.
+        chats: {
+            collection: 'Chat',
+            via: 'users'
+        },
+
         // The single Profile that this User owns.
         profile: {
             model: 'Profile',
@@ -44,9 +50,8 @@ module.exports = {
 
     },
 
-
     // Called before a User model is created, will hash the password; returns error if hashing fails.
-    beforeCreate: function (values, cb) {
+    beforeCreate: (values, cb) => {
         // Hash password
         bcrypt.hash(values.password, 10, function(err, hash) {
             if(err) return cb(err);
@@ -56,24 +61,24 @@ module.exports = {
     },
 
     // After a User has been created, create them a Profile.
-    afterCreate: function(newlyInsertedRecord, cb) {
+    afterCreate: (newlyInsertedRecord, cb) => {
         Profile.create({ username: newlyInsertedRecord.username, owner: newlyInsertedRecord.id }).exec(cb);
     },
     
     // After a User's credentials have been updated, de-auth all their devices.
-    afterUpdate: function(updatedRecord, cb) {
+    afterUpdate: (updatedRecord, cb) => {
         Device.destroy({owner: updatedRecord.id}).exec(cb);
     },
 
     // Events to trigger when a User is destroyed.
-    afterDestroy: function(destroyedRecords, cb) {
+    afterDestroy: (destroyedRecords, cb) => {
         var userID = _.pluck(destroyedRecords, 'id');
         // Destroy all this Users data. 
         Device.destroy({owner: userID}).exec(cb);
         Profile.destroy({owner: userID}).exec(cb);
     },
 
-    /* Method takes two users, and returns them (if they exist).
+   /* Method takes two users, and returns them (if they exist).
     *  
     * sender:   JSON representation of the User that sent a specific request.
     * receiver: JSON representation of the User that has/is to receive the request
