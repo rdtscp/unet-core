6f35f445d0c44cbdcd92107b27bfa96d4f677ea3
Alexander Wilson <acwilson96@gmail.com>
2017-09-06 02:07:02 +0100

    Bugfix

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -96,7 +96,7 @@ module.exports = {
             var userOne = members[0];
             var userTwo = members[1];
             Chat.chatExists(userOne, userTwo, (exists) => {
-                if (!exist) {
+                if (!exists) {
                     // @TODO Check that requesting user is indeed friends with all members.
                     Chat.create({
                         name: undefined
