eb51ead4e163b7b1d74890feff5bb1cc1ebadcad
Alexander Wilson <acwilson96@gmail.com>
2017-09-06 20:27:10 +0100

    Getting a User model now returns full chat data.

--- api/models/Chat.js
+++ api/models/Chat.js
@@ -69,40 +69,33 @@ module.exports = {
         });
     },
 
-    // Returns input list with new attribute 'friend' which contains the model for the friend.
-    getFriend(currUser, chats, cb) {
-        var tasks = [];
-        var output = [];
-        for (var i=0; i < chats.length; i++) {
-            var chat = chats[i];
-            switch (currUser.id) {
-                // If the requesting user is user_one, return user_two's User model.
-                case chat.user_one:
-                    tasks.push(
-                        User.findOne({id: chat.user_two}).then((user) => {
-                            var out_chat = chat;
-                            out_chat.friend = { username: user.username, id: user.id };
-                            output.push(out_chat);
-                            return;
-                        })
-                    );
-                // Else the user is user_two, return user_one's User model.
-                default:
-                    tasks.push(
-                        User.findOne({id: chat.user_one}).then((user) => {
-                            var out_chat = chat;
-                            out_chat.friend = { username: user.username, id: user.id };
-                            output.push(out_chat);
-                            return;
-                        })
-                    );
-            }
+    // Takes a user, and returns its chats with relative chat names.
+    populateChatNames(user, cb) {
+        var output   = [];
+        var requests = [];
+        for (var i=0; i < user.chats.length; i++) {
+            requests.push(
+                Chat.findOne({
+                    id: user.chats[i].id
+                }).populate('users').then((currChat) => {
+                    if (currChat.users[0].id == user.id) {
+                        currChat.name = currChat.users[1].username;
+                    } else {
+                        currChat.name = currChat.users[0].username;
+                    }
+                    output.push(currChat);
+                    return currChat;
+                })
+            );
         }
         return Promise
-        .all(tasks)
-        .then(result => {
-            cb(output);
-        });
+                .all(requests)
+                .then(result => {
+                    cb(null, output); 
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
     },
 
     // Retrieves the next 10 messages starting from message# startingNum in chat chatID.
