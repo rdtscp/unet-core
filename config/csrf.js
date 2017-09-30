da94bf743177566357628c870c56505f5b4af106
Alex Wilson <acwilson96@protonmail.com>
2017-09-30 11:58:40 +0100

    Allow localhost clients

--- config/csrf.js
+++ config/csrf.js
@@ -60,5 +60,5 @@ module.exports.csrf = true;
 
 module.exports.csrf = {
    grantTokenViaAjax: true,
-   origin: 'https://acwilson96.github.io'
+   origin: 'https://acwilson96.github.io, http://localhost'
 }
